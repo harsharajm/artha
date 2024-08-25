d5ede2633c3448519da61f48b4bee6e47542be2d
Meerjumla Harshavardhan Raj <mharsha024@gmail.com>
2024-08-25 12:11:19 +0530

    Removed useEffect

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -3,19 +3,18 @@ import './Popup.css';
 import { generateMnemonic } from 'bip39';
 
 function Popup({setView}) {
-  const [mnemonic, setMnemonic] = useState('');
-
-  // Use useEffect to avoid setting state directly in the render phase
-  useEffect(() => {
-    setMnemonic(generateMnemonic());
-  }, []); // Empty dependency array ensures this runs only once
-
+  const mnemonic = generateMnemonic();
   return (
     <>
-    <div className="popup" onClick={()=>{
-      navigator.clipboard.writeText(mnemonic);
+    <div className="popup" >
+
+    <div className="copy" style={{ display: 'flex',justifyContent:'space-around'
     }}>
+
       <h3>Recovery Phrase</h3>
+      <button style={{height:'23px'}} onClick={()=>{navigator.clipboard.writeText(mnemonic);}} >Copy</button>
+    
+      </div>
       <div className="words">
         {mnemonic.split(' ').map((word) => (
           <div className="word">{word}</div>
@@ -23,6 +22,7 @@ function Popup({setView}) {
         ))}
         <button onClick={()=>setView('dashboard')}>Continue</button>
       </div>
+
     </div>
     </>
 
